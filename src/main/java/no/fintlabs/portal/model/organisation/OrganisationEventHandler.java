5e62fcc8383227c162519aeadc2325279909009c
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-03-10 13:00:03 +0100

    FLA-100 We now produce an event after creating an organisation.

--- src/main/java/no/fintlabs/portal/model/organisation/OrganisationEventHandler.java
+++ src/main/java/no/fintlabs/portal/model/organisation/OrganisationEventHandler.java
@@ -2,6 +2,9 @@ package no.fintlabs.portal.model.organisation;
 
 import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.kafka.event.EventConsumerFactoryService;
+import no.fintlabs.kafka.event.EventProducer;
+import no.fintlabs.kafka.event.EventProducerFactory;
+import no.fintlabs.kafka.event.EventProducerRecord;
 import no.fintlabs.kafka.event.topic.EventTopicNameParameters;
 import no.fintlabs.kafka.event.topic.EventTopicService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -12,36 +15,57 @@ import java.time.Duration;
 
 @Slf4j
 @Component
-public class OrganisationConsumer {
+public class OrganisationEventHandler {
 
     private final EventTopicService eventTopicService;
     private final EventConsumerFactoryService consumer;
     private final OrganisationService organisationService;
 
-    public OrganisationConsumer(EventTopicService eventTopicService, EventConsumerFactoryService consumer, OrganisationService organisationService) {
+    private final EventProducer<Organisation> eventProducer;
+    private final EventTopicNameParameters organisationCreatedTopic;
+
+    public OrganisationEventHandler(EventTopicService eventTopicService, EventConsumerFactoryService consumer, OrganisationService organisationService, EventProducerFactory eventProducerFactory) {
         this.eventTopicService = eventTopicService;
         this.consumer = consumer;
         this.organisationService = organisationService;
+
+        eventProducer = eventProducerFactory.createProducer(Organisation.class);
+        organisationCreatedTopic = EventTopicNameParameters
+                .builder()
+                .orgId("flais.io")       // Optional if set as application property
+                .domainContext("fint-service")  // Optional if set as application property
+                .eventName("organisation-created")
+                .build();
+
+
     }
+
 
     @PostConstruct
     public void init() {
-        EventTopicNameParameters topic = EventTopicNameParameters
+        EventTopicNameParameters createOrganisationTopic = EventTopicNameParameters
                 .builder()
                 .orgId("flais.io")       // Optional if set as application property
                 .domainContext("fint-service")  // Optional if set as application property
                 .eventName("new-organisation")
                 .build();
-        eventTopicService.ensureTopic(topic,
-                Duration.ofHours(48).toMillis());
+        eventTopicService.ensureTopic(createOrganisationTopic, Duration.ofHours(48).toMillis());
+        eventTopicService.ensureTopic(organisationCreatedTopic, Duration.ofHours(48).toMillis());
 
         consumer.createFactory(Organisation.class, this::processEvent)
-                .createContainer(topic);
+                .createContainer(createOrganisationTopic);
+
     }
 
     private void processEvent(ConsumerRecord<String, Organisation> consumerRecord) {
         log.info("New organisation event received for : {}", consumerRecord.value().getDisplayName());
         Organisation organisation = organisationService.createOrganisation(consumerRecord.value());
+        eventProducer.send(EventProducerRecord
+                .<Organisation>builder()
+                .topicNameParameters(organisationCreatedTopic)
+                .value(organisation)
+                .build()
+        );
         log.info("Created new organisation {}", organisation);
     }
 

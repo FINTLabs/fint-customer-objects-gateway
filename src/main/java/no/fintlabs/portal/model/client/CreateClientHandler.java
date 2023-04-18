a5e57c112caf61ea7b12ee465f009f7a2bd2bd78
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-04-18 12:28:29 +0200

    Improved client create handler.

--- src/main/java/no/fintlabs/portal/model/client/CreateClientHandler.java
+++ src/main/java/no/fintlabs/portal/model/client/CreateClientHandler.java
@@ -37,46 +37,57 @@ public class CreateClientHandler extends FintCustomerObjectWithSecretsRequestHan
     public Client apply(ConsumerRecord<String, ClientEvent> consumerRecord, Organisation organisation) {
         log.info("{} event", consumerRecord.value().getOperationWithType());
 
+        return getOrCreateClient(consumerRecord, organisation);
+    }
+
+    private Client getOrCreateClient(ConsumerRecord<String, ClientEvent> consumerRecord, Organisation organisation) {
         String clientDn = clientFactory.getClientDn(consumerRecord.value().getObject().getName(), organisation);
-        Client object = consumerRecord.value().getObject();
-        object.setDn(clientDn);
+        Client client = consumerRecord.value().getObject();
+        client.setDn(clientDn);
 
-        return getFromCache(object)
-                .map(client -> {
-                    log.debug("Found client in cache {}", client.getDn());
-                    return client;
-                })
-                .orElseGet(() -> {
-                    log.debug("Client {} not in cache", consumerRecord.value().getObject().getName());
-                    return clientService
-                            .getClientByName(
-                                    consumerRecord.value().getObject().getName(),
-                                    organisation)
-                            .map(client -> {
-                                log.debug("The client ({}) already exists", client.getDn());
-                                client.setPublicKey(consumerRecord.value().getObject().getPublicKey());
-                                clientService.resetClientPassword(client, consumerRecord.value().getObject().getPublicKey());
-                                clientService.encryptClientSecret(client, consumerRecord.value().getObject().getPublicKey());
-                                addToCache(client);
-                                return client;
-                            })
-                            .orElseGet(() -> {
-                                log.debug("Client {} don't exist. Creating client...", consumerRecord.value().getObject().getName());
-                                Client client = clientService.addClient(consumerRecord.value().getObject(), organisation)
-                                        .orElseThrow(() -> new RuntimeException("An unexpected error occurred while creating client."));
+        return getFromCache(client)
+                .map(CreateClientHandler::handleFoundInCache)
+                .orElseGet(() -> handleNotFoundInCache(consumerRecord, organisation));
+    }
 
-                                client.setPublicKey(consumerRecord.value().getObject().getPublicKey());
-                                clientService.resetClientPassword(client, consumerRecord.value().getObject().getPublicKey());
-                                clientService.encryptClientSecret(client, consumerRecord.value().getObject().getPublicKey());
-                                addToCache(client);
+    private static Client handleFoundInCache(Client client) {
+        log.debug("Found client in cache {}", client.getDn());
+        return client;
+    }
 
-                                return client;
-                            });
+    private Client handleNotFoundInCache(ConsumerRecord<String, ClientEvent> consumerRecord, Organisation organisation) {
+        log.debug("Client {} not in cache", consumerRecord.value().getObject().getName());
+        return clientService
+                .getClientByName(
+                        consumerRecord.value().getObject().getName(),
+                        organisation)
+                .map(client -> handleGetClientSuccess(consumerRecord, client))
+                .orElseGet(() -> handleCreateClient(consumerRecord, organisation));
+    }
 
+    private Client handleCreateClient(ConsumerRecord<String, ClientEvent> consumerRecord, Organisation organisation) {
+        log.debug("Client {} don't exist. Creating client...", consumerRecord.value().getObject().getName());
+        Client client = clientService.addClient(consumerRecord.value().getObject(), organisation)
+                .orElseThrow(() -> new RuntimeException("An unexpected error occurred while creating client."));
 
+        ensureSecrets(consumerRecord, client);
+        ensureComponents(consumerRecord);
+        addToCache(client);
 
+        return client;
+    }
 
-                });
+    private Client handleGetClientSuccess(ConsumerRecord<String, ClientEvent> consumerRecord, Client client) {
+        log.debug("The client ({}) already exists", client.getDn());
+        ensureSecrets(consumerRecord, client);
+        addToCache(client);
+        return client;
+    }
+
+    private void ensureSecrets(ConsumerRecord<String, ClientEvent> consumerRecord, Client client) {
+        client.setPublicKey(consumerRecord.value().getObject().getPublicKey());
+        clientService.resetClientPassword(client, consumerRecord.value().getObject().getPublicKey());
+        clientService.encryptClientSecret(client, consumerRecord.value().getObject().getPublicKey());
     }
 
     private void ensureComponents(ConsumerRecord<String, ClientEvent> consumerRecord) {

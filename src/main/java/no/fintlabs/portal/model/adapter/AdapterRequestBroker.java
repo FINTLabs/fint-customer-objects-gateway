d4c586a5ef89ac326df753f3d12ae10c728ec480
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-05-11 11:40:40 +0200

    FLA-117 Fixed issue with consumer container.

--- src/main/java/no/fintlabs/portal/model/adapter/AdapterRequestBroker.java
+++ src/main/java/no/fintlabs/portal/model/adapter/AdapterRequestBroker.java
@@ -1,6 +1,8 @@
 package no.fintlabs.portal.model.adapter;
 
 import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.kafka.requestreply.RequestConsumerFactoryService;
+import no.fintlabs.kafka.requestreply.topic.RequestTopicService;
 import no.fintlabs.portal.model.FintCustomerObjectHandler;
 import no.fintlabs.portal.model.FintCustomerObjectRequestBroker;
 import no.fintlabs.portal.model.organisation.OrganisationService;
@@ -11,7 +13,8 @@ import java.util.Collection;
 @Slf4j
 @Component
 public class AdapterRequestBroker extends FintCustomerObjectRequestBroker<AdapterEvent, Adapter> {
-    public AdapterRequestBroker(OrganisationService organisationService, Collection<FintCustomerObjectHandler<Adapter, AdapterEvent>> fintCustomerObjectHandlers) {
-        super(organisationService, fintCustomerObjectHandlers);
+    public AdapterRequestBroker(OrganisationService organisationService, Collection<FintCustomerObjectHandler<Adapter, AdapterEvent>> fintCustomerObjectHandlers,RequestConsumerFactoryService requestConsumerFactoryService,
+                                RequestTopicService requestTopicService) {
+        super(organisationService, fintCustomerObjectHandlers, requestConsumerFactoryService, requestTopicService);
     }
 }
--- src/main/java/no/fintlabs/portal/model/client/ClientRequestBroker.java
+++ src/main/java/no/fintlabs/portal/model/client/ClientRequestBroker.java
@@ -1,8 +1,10 @@
 package no.fintlabs.portal.model.client;
 
 import lombok.extern.slf4j.Slf4j;
-import no.fintlabs.portal.model.FintCustomerObjectRequestBroker;
+import no.fintlabs.kafka.requestreply.RequestConsumerFactoryService;
+import no.fintlabs.kafka.requestreply.topic.RequestTopicService;
 import no.fintlabs.portal.model.FintCustomerObjectHandler;
+import no.fintlabs.portal.model.FintCustomerObjectRequestBroker;
 import no.fintlabs.portal.model.organisation.OrganisationService;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +13,9 @@ import java.util.Collection;
 @Slf4j
 @Component
 public class ClientRequestBroker extends FintCustomerObjectRequestBroker<ClientEvent, Client> {
-    public ClientRequestBroker(OrganisationService organisationService, Collection<FintCustomerObjectHandler<Client, ClientEvent>> fintCustomerObjectHandlers) {
-        super(organisationService, fintCustomerObjectHandlers);
+
+    public ClientRequestBroker(OrganisationService organisationService, Collection<FintCustomerObjectHandler<Client, ClientEvent>> fintCustomerObjectHandlers, RequestConsumerFactoryService requestConsumerFactoryService,
+                               RequestTopicService requestTopicService) {
+        super(organisationService, fintCustomerObjectHandlers, requestConsumerFactoryService, requestTopicService);
     }
 }
